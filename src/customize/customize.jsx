bb0d81b00b94a25841441934a29151b771a40418
Katia Jensen <katiajensen26@gmail.com>
2025-10-06 22:22:32 -0600

    Completed 'Create View Components' section of React part 1

---
+++ src/customize/customize.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Customize() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>customize displayed here</div>
+    </main>
+  );
+}
