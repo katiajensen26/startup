baaeaea2ec3e4ee8c86b87ad31463066b08b28b7
Katia Jensen <katiajensen26@gmail.com>
2025-10-16 23:01:13 -0600

    Adjusted friends page to be interactive and opens up your friend's bookshelf.

--- src/friends/friends.jsx
+++ src/friends/friends.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import './friends.css';
 
 export function Friends() {
+  const [friendURL, setFriendURL] = React.useState('');
+
+  function handleURL(event) {
+    event.preventDefault();
+
+    if (friendURL) {
+      window.open(friendURL, '_blank');
+    }
+  }
+
   return (
     <main className="friends-page">
 
         <h1>Visit a friend's bookshelf!</h1>
 
         <p>Enter your friend's url below:</p>
-        <form method="get" action="" target="_blank">
-            <input type="url" name="friends-url" placeholder="https://friend-bookshelf.com" required size="30" />
-            <button type="submit" class="custom-btn">Visit!</button>
-        </form>
+          <form onSubmit={handleURL}>
+            <input type="url" name="friends-url" placeholder="https://friend-bookshelf.com" required size="30" value={friendURL} onChange={(e) => setFriendURL(e.target.value)} />
+            <button type="submit" className="custom-btn">Visit!</button>
+          </form>
     </main>
   );
 }
