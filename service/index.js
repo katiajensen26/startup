c3986d701ca06fb8f84a2335992da075e3997957
Katia Jensen <katiajensen26@gmail.com>
2025-11-02 21:27:59 -0700

    Started adding authentication endpoints

--- service/index.js
+++ service/index.js
@@ -1,11 +1,71 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
-app.get('*', (_req, res) =>{
-    res.send({msg: 'MyBookshelf service'});
+app.use(express.json());
+
+let users = [];
+let bookshelves = [];
+
+let apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('email', req.body.email)) {
+        res.status(409).send({msg: 'Existing user'});
+    } else {
+        const user = await createUser(req.body.email, req.body.password);
+
+        setAuthCookie(res, user.token);
+        res.send({ email: user.email });
+    }
 });
+
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+apiRouter.delete('/auth/logout', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
+
+const verifyAuth = async (req, res, next) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
+};
+
+//Add bookshelf stuff here?
+
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+});
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
