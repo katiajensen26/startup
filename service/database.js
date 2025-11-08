e806300651b037390fee79acd4e1adf05d8b2933
Katia Jensen <katiajensen26@gmail.com>
2025-11-08 16:41:32 -0700

    installed mongodb and added user implementation in database.js

---
+++ service/database.js
@@ -0,0 +1,41 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('MyBookshelf');
+const userCollection = db.collection('user');
+//add bookshelves here later
+
+(async function testConnection() {
+    try {
+        await db.command({ ping: 1 });
+    } catch (ex) {
+        console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+        process.exit(1);
+    }
+})();
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+    await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+    await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+
+module.exports = {
+    getUser,
+    getUserByToken,
+    addUser,
+    updateUser,
+};
