e69245a3843723f254b6fa713a017c94aaf7a67b
Katia Jensen <katiajensen26@gmail.com>
2025-11-08 23:52:29 -0700

    Started implementing the database storage for bookshelves.

--- service/database.js
+++ service/database.js
@@ -4,8 +4,8 @@ const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('MyBookshelf');
-const userCollection = db.collection('user');
-//add bookshelves here later
+const userCollection = db.collection('users');
+const Bookshelves = db.collection('bookshelves');
 
 (async function testConnection() {
     try {
@@ -32,10 +32,34 @@ async function updateUser(user) {
     await userCollection.updateOne({ email: user.email }, { $set: user });
 }
 
+async function getBookshelfByUser(user) {
+    await Bookshelves.findOne({ email: user.email });
+}
+
+async function getBookshelfbyShareId(shareID) {
+    await Bookshelves.findOne({ shareID });
+}
+
+async function createOrUpdateBookshelf(user, bookshelfData) {
+    const update = {
+        $set: {
+            shelfName: bookshelfData.name || "My Bookshelf",
+            books: bookshelfData.books || [],
+            isPublic: bookshelfData.isPublic ?? true
+        },
+        $setOnInsert: {
+            email: user.email,
+            shareID: bookshelfData.shareID || generateShareId()
+        }
+    }
+}
 
 module.exports = {
     getUser,
     getUserByToken,
     addUser,
     updateUser,
+    getBookshelfByUser,
+    getBookshelfbyShareId,
+    createOrUpdateBookshelf
 };
